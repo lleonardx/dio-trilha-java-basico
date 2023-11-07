1389c697868c3c124fb2750ea8a0dc515df9e0b3
lleonardx <lleonardx.alves@gmail.com>
2023-11-07 10:16:57 -0400

    operadores unitarios

---
+++ tipos-variaveis/src/OperadorUnario.java
@@ -0,0 +1,14 @@
+public class OperadorUnario {
+    public static void main(String[] args) {
+        //numero negativo
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+
+        //numero positivo
+        //conversão de multiplicar por -1
+        numero = numero * -1;
+        System.out.println(numero);
+    }
+    
+}
