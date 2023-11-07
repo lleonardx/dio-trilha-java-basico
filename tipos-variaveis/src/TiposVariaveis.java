0fb3115a6138d895e32e9e130da442c8daff388c
lleonardx <lleonardx.alves@gmail.com>
2023-11-07 09:50:29 -0400

    aplicando tipos de variaveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // variaveis-valor armazenado em memoria 
+        //vs 
+        //constantes-valor armazenado em memoria que não pode ser mudados
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        
+        //Para declarar a constante, deve ter a variavel em CAPS e o "final"
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
+
     }
 }
