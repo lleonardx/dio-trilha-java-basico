468c5376b26c8bba2537911317cf6396fee077f6
lleonardx <lleonardx.alves@gmail.com>
2023-11-07 10:56:44 -0400

    update de operadores unitarios e ternarios

--- tipos-variaveis/src/OperadorTernario.java
+++ tipos-variaveis/src/OperadorTernario.java
@@ -1,6 +1,22 @@
 public class OperadorTernario {
     public static void main(String[] args) {
+        //operador ternario
+        int a,b;
+        a=5;
+        b=6;
+
+        //maneiras de expressar o operador ternario
+        String resultado = "";
+        if(a==b){
+            resultado = "verdadeiro";
+        }else{
+            resultado = "falso";
+        }
+        System.out.println(resultado);
         
+        
+        String resultado1 = a==b ? "verdeiro" : "falso";
+        System.out.println(resultado1);
     }
     
 }
