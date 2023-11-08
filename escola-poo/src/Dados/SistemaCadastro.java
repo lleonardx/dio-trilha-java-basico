94f3c4cbc1015f75ffa19f8cfe20a07b588f8819
lleonardx <lleonardx.alves@gmail.com>
2023-11-08 14:40:04 -0400

    construtores

---
+++ escola-poo/src/Dados/SistemaCadastro.java
@@ -0,0 +1,13 @@
+package Dados;
+
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa("Marcos", "123456");
+        System.out.println(marcos.getNome() + marcos.getCpf());
+
+
+
+
+    }
+    
+}
