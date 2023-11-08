6e5d75b7c6c2b90b1ac1c2dc53ebebeb9ca3ff64
lleonardx <lleonardx.alves@gmail.com>
2023-11-08 15:41:38 -0400

    abstração e polimorfismo

---
+++ pilares-poo/src/Autodromo.java
@@ -0,0 +1,13 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.setChassi("879879");
+        jeep.ligar();
+
+        Moto z400 = new Moto();
+        z400.setChassi("467754");
+        z400.ligar();
+
+
+    }
+}
