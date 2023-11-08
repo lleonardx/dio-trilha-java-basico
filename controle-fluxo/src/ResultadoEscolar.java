df35bf850934407d6fd4dd9f2cccc1fbe67d1dc3
lleonardx <lleonardx.alves@gmail.com>
2023-11-08 08:20:23 -0400

    condição ternaria

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
-       
-        if (nota >= 7)
-		System.out.println("Aprovado");
-
-	    else if (nota >= 5 && nota < 7)
-		System.out.println("Recuperação");
-        
-	    else
-		System.out.println("Reprovado");
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
     }
 }
